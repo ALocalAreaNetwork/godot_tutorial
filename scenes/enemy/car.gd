8d25c5c090c30842af60fce5b3e7e22487b37713
ALocalAreaNetwork <alangetsjunk@gmail.com>
2024-10-09 16:54:33 -0500

    add car enemy add turret to car add turret following enemy within set range add turret targeting lines for when charging at the player add turret shoots after charging for a second add a start up time of one second before the laser begins to charge (total 2 seconds to fire at player)

---
+++ scenes/enemy/car.gd
@@ -0,0 +1,28 @@
+extends PathFollow2D
+
+var player_near: bool = false
+
+@onready var turret_left_ray: RayCast2D = $Turret/RayCast2D
+@onready var turret_left_line: Line2D = $Turret/RayCast2D/Line2D2
+@onready var turret_right_ray: RayCast2D = $Turret/RayCast2D2
+@onready var turret_right_line: Line2D = $Turret/RayCast2D2/Line2D2
+
+
+func fire():
+	Globals.health -= 20
+
+func _ready():
+	turret_right_line.add_point(turret_right_ray.target_position)
+
+func _process(delta):
+	progress_ratio += 0.01 * delta
+	if player_near:
+		$Turret.look_at(Globals.player_pos)
+		print($Turret/RayCast2D.get_collider())
+
+func _on_attack_area_body_entered(_body: Node2D) -> void:
+	player_near = true
+	$AnimationPlayer.play('laser_charge')
+
+func _on_attack_area_body_exited(_body: Node2D) -> void:
+	player_near = false
